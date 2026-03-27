54f3675b1279ef66bf5b8fa3ae89c49e16daed1a
Gianluca Rea <reagianluca97@gmail.com>
2026-03-27 14:48:09 +0100

    Implement First Iteration of REPL

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		text = strings.ToLower(text)
+		words := strings.Fields(text)
+		fmt.Println("Your command was:", words[0])
+	}
 }
---
+++ tests/repl_test.go
@@ -0,0 +1,44 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/gianlucarea/pokedex/repl"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    " HELLO WORLD AGAIN",
+			expected: []string{"HELLO", "WORLD", "AGAIN"},
+		},
+		{
+			input:    "HELLO XYZ",
+			expected: []string{"HELLO", "XYZ"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := repl.CleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length mismatch for input %q: got %d words, want %d", c.input, len(actual), len(c.expected))
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("Content mismatch at index %d for input %q: got %q, want %q", i, c.input, word, expectedWord)
+			}
+		}
+	}
+}

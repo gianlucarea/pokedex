4cfcbbbfa2f2f1d25ca76d716bb7db1551d15028
Gianluca Rea <reagianluca97@gmail.com>
2026-03-27 16:14:03 +0100

    Finish Repl

--- main.go
+++ main.go
@@ -7,6 +7,25 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var supportedCMD map[string]cliCommand = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    cmdExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Help Function of the Pokedex",
+		callback:    cmdHelp,
+	},
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -15,6 +34,22 @@ func main() {
 		text := scanner.Text()
 		text = strings.ToLower(text)
 		words := strings.Fields(text)
-		fmt.Println("Your command was:", words[0])
+		cmd, ok := supportedCMD[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		cmd.callback()
 	}
+}
+
+func cmdExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func cmdHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
 }
